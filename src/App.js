7ca896f0f24bf8f1ad5afa4b4f543741790fa59f
fcarlone <frankcarlone@gmail.com>
2019-06-20 11:23:42 -0400

    Insert OAuth data and store in .env file

--- src/App.js
+++ src/App.js
@@ -22,7 +22,11 @@ class App extends React.Component {
   async componentDidMount() {
     this.setState({ loading: true });
 
-    const res = await axios.get("https://api.github.com/users");
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
     console.log(res.data);
 
     this.setState({
