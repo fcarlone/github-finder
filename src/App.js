34fdea7f9dfec6e9642b8f2b303cf0f24f6ff48a
fcarlone <frankcarlone@gmail.com>
2019-06-24 09:29:08 -0400

    Add React Router to project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import User from "./components/users/User";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
 import axios from "axios";
 import "./App.css";
 
@@ -10,34 +13,19 @@ class App extends React.Component {
   // Store user to state
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
 
   // Request to the GitHub API
-
   // componentDidMount() {
   // axios.get("https://api.github.com/users").then(response => {
   //   console.log(response.data);
   // });
 
-  // Using Async/Await
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-
-  //   const res = await axios.get(
-  //     `https://api.github.com/users?client_id=${
-  //       process.env.REACT_APP_GITHUB_CLIENT_ID
-  //     }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-  //   );
-
-  //   this.setState({
-  //     users: res.data,
-  //     loading: false
-  //   });
-  // }
-
   // Search GitHub Users
+  // Using Async/Await
   searchUsersApp = async text => {
     this.setState({ loading: true });
 
@@ -50,6 +38,23 @@ class App extends React.Component {
     console.log("search axios get request", res.data);
     this.setState({
       users: res.data.items,
+      loading: false
+    });
+  };
+
+  // Get a single GitHub user
+  getUser = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    console.log("search axios get request", res.data);
+    this.setState({
+      user: res.data,
       loading: false
     });
   };
@@ -80,19 +85,47 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsersApp}
-            clearUsers={this.clearUsersApp}
-            showClear={this.state.users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={this.state.loading} users={this.state.users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsersApp}
+                      clearUsers={this.clearUsersApp}
+                      showClear={this.state.users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users
+                      loading={this.state.loading}
+                      users={this.state.users}
+                    />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/user/:login"
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={this.state.user}
+                    loading={this.state.loading}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,8 +2,9 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+const UserItem = ({ user: { login, avatar_url } }) => {
   // Destructure State
   // const { login, avatar_url, html_url } = props.user;
 
@@ -17,9 +18,9 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
       />
       <h3>{login}</h3>
       <div>
-        <a href={html_url} className="btn btn-dark sm my-1">
+        <Link to={`/user/${login}`} className="btn btn-dark sm my-1">
           More
-        </a>
+        </Link>
       </div>
     </div>
   );
